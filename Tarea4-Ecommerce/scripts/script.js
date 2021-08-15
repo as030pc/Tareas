e90151dfca5f80cdc17a6802bf4b0f8f6e4fa580
Andres Palma <palma.castaneda015@gmail.com>
2021-08-14 22:59:59 -0500

    e-commerce

---
+++ Tarea4-Ecommerce/scripts/script.js
@@ -0,0 +1,5 @@
+function adicionarCarrito () {
+    let fragment = document.createDocumentFragment;
+    let template = document.getElementById('lista-compra').content;
+    template.querySelector('p').textContent = pelicula
+}
