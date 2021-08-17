7cdd22b759b28e7f1decdabfeb111aef57e7728d
Andres Palma <palma.castaneda015@gmail.com>
2021-08-16 22:54:10 -0500

    Cambios ejercicioPeliculas

--- Tarea2Peliculas/script.js
+++ Tarea2Peliculas/script.js
@@ -2,11 +2,11 @@
 const URL_Movies= "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1"
 const imageMovies = `https://image.tmdb.org/t/p/w1280`
 let template1 = document.querySelector('#template').content;
-let peliculasAll  = document.getElementById('peliculasAll');
 let fragment = document.createDocumentFragment();
 //let descripcion = document.createDocumentFragment();
 const items = document.getElementById('items');
 const detail = document.getElementById('detail');
+let pelicula = {};
 
 
 async function obtenerPeliculas(url) {
@@ -27,17 +27,126 @@ obtenerPeliculas(URL_Movies)
 const loadData = data => {
     data.forEach(pelicula => {
         const{id, title, poster_path, overview}= pelicula;
+       
         template1.querySelector('h5').textContent = title
         template1.querySelector('img').setAttribute('src', imageMovies + poster_path)
-        //template1.querySelector('a').setAttribute('href',descripcion)
-        const clone = template1.cloneNode(true);
         template1.querySelector('img').dataset.id = id;
+        template1.querySelector('h6').textContent = overview;
+        const clone = template1.cloneNode(true);
+        
         fragment.appendChild(clone)
     })
-    peliculasAll.appendChild(fragment);
+    items.appendChild(fragment);
+
+    items.addEventListener('click', e => {
+    
+    console.log(e.target.dataset.id);
+    let idTarget = e.target.dataset.id;
+    data.forEach(pelicula => {
+        const {id, title, poster_path, overview} = pelicula;
+        
+        if(id == idTarget){
+            
+            
+            const objeto = {
+                id: id,
+                title: title,
+                poster_path: poster_path,
+                overview: overview,
+    
+            }
+            
+            localStorage.setItem("Pelicula",JSON.stringify(objeto));
+            getPelicula();
+  
+        }   
+    })
+    e.stopPropagation();
+    e.preventDefault();
+ })
+ 
+ 
+ function getPelicula(){
+     detail.innerHTML = '';
+     
+     pelicula = JSON.parse(localStorage.getItem("Pelicula")); 
+    
+     const {id, title, poster_path,overview} = pelicula;
+     
+     detail.innerHTML = `
+     <table border="2px" align="center">
+     <tr>
+     
+         <td align="center">
+          <h2>${title}</h2>
+          <h4>${overview}</h4>
+
+         </td>
+     </tr>
+ </table>
+     `
+ }
+
+    
 }
+ 
+// items.addEventListener('click', e => {
+    
+//     console.log(e.target.dataset.id);
+//     let idTarget = e.target.dataset.id;
+//     data.forEach(pelicula => {
+//         const {id, title, poster_path, overview} = pelicula;
+        
+//         if(id == idTarget){
+            
+            
+//             const objeto = {
+//                 id: id,
+//                 title: title,
+//                 poster_path: poster_path,
+//                 overview: overview,
+    
+//             }
+            
+//             localStorage.setItem("Pelicula",JSON.stringify(objeto));
+//             getPelicula();
+  
+//         }   
+//     })
+//     e.stopPropagation();
+//     e.preventDefault();
+//  })
+ 
+ 
+//  function getPelicula(){
+//      detail.innerHTML = '';
+     
+//      pelicula = JSON.parse(localStorage.getItem("Pelicula")); 
+    
+//      const {id, title, poster_path,overview} = pelicula;
+     
+//      detail.innerHTML = `
+//      <table border="2px" align="center">
+//      <tr>
+         
+//          <td align="center">
+//           <h2>${title}</h2>
+//           <h4>${overview}</h4>
+
+//          </td>
+//      </tr>
+//  </table>
+//      `
+//  }
 
 
+
+
+
+
+
+
+/*
 items.addEventListener('click', e => {
     let idObjetivo = e.target.dataset.id;
     console.log(e.target.dataset.id);
@@ -64,6 +173,8 @@ items.addEventListener('click', e => {
     e.preventDefault();
     
 })
+
+*/
 
 boton.addEventListener('click', function () {
     let nombre = document.querySelector('.nombre').value;
